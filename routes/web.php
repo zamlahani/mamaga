d0e1fea5ec5314d2eb0c7cea561418bfe788cd86
Khussal Zamlahani <zamlahani@gmail.com>
2019-05-16 22:53:45 +0700

    disable register new user & reset password

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes([
+	'register' => false,
+	'reset'    => false,
+]);
 
 Route::get('/home', 'HomeController@index')->name('home');
